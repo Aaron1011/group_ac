11a8562356a116fc6f327d0de0061ab2079f1086
Aaron Hill <aa1ronham@gmail.com>
2024-09-18 13:08:16 -0400

    Prove that union is open

--- GroupAc/MyDeriv.lean
+++ GroupAc/MyDeriv.lean
@@ -1,6 +1,20 @@
 import Mathlib
+import Mathlib.Data.Set.Defs
+import Mathlib.Data.Set.Basic
 
 variable {f}
 
-theorem infinite_zero_is_poly (hf: ∀ (x : ℝ), x ∈ (Set.Icc 0 1) → ∃ (n: ℕ), (iteratedFDerivWithin n f (Set.Icc 0 1)) x) : IsPoly f :=
-  sorry
+def RestrictsToPoly (f: ℝ → ℝ) (a b: ℝ) :=
+  ∃ (p: Polynomial ℝ), ∀ (y: ℝ), y ∈ Set.Icc a b → f y = p.eval y
+
+-- https://mathoverflow.net/questions/34059/if-f-is-infinitely-differentiable-then-f-coincides-with-a-polynomial
+theorem infinite_zero_is_poly (hf: ∀ (x : ℝ), x ∈ (Set.Icc 0 1) → ∃ (n: ℕ), (iteratedFDerivWithin ℝ n f (Set.Icc 0 1)) x = 0): RestrictsToPoly f 0 1 := by
+  -- let real_powset := 𝒫 { z: ℝ | True }
+  let poly_union := Set.sUnion { i | ∃ (a b : ℝ ), i = Set.Ioo a b ∧ RestrictsToPoly f a b }
+  have poly_open: IsOpen poly_union := by
+    apply isOpen_sUnion
+    intro t ht
+    simp at ht
+    obtain ⟨a, b, h, h'⟩ := ht
+    rw [h]
+    apply isOpen_Ioo
