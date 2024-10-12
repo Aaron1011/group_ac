3e461754ce9200471d8ec918f8381f29dfe3e199
Aaron Hill <aa1ronham@gmail.com>
2024-10-12 14:05:20 -0400

    Generalize away from R

--- GroupAc/NotOpen.lean
+++ GroupAc/NotOpen.lean
@@ -8,13 +8,17 @@ import Mathlib.Order.Filter.Basic
 open Topology
 open Filter
 
-lemma general_iic_not_open  (a: ℝ) (hb: ∃ b, b ∉ (Set.Iic a)) : ¬ IsOpen (Set.Iic a) := by
+lemma general_iic_not_open  {α : Type u} [TopologicalSpace α] [LinearOrder α] [OrderClosedTopology α] [PreconnectedSpace α]  (a: α) (ha: ¬ IsMax a) : ¬ IsOpen (Set.Iic a) := by
   have is_closed: IsClosed (Set.Iic a) := isClosed_Iic
   have nonempty: (Set.Iic a).Nonempty := by simp
 
   have ici_not_univ: Set.Iic a ≠ Set.univ := by
     rw [Set.ne_univ_iff_exists_not_mem]
+    obtain ⟨b, hb⟩ := not_isMax_iff.mp ha
+    use b
+    simp
     exact hb
+
 
   have not_clopen: ¬IsClopen (Set.Iic a) := by
     apply (not_imp_not.mpr isClopen_iff.mp)
