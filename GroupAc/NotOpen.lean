46e9cd49b7fa97d22a392d8fef7c83789efee823
Julian Berman <Julian@GrayVines.com>
2024-10-12 15:35:27 -0400

    Golf golf golf

--- GroupAc/NotOpen.lean
+++ GroupAc/NotOpen.lean
@@ -1,25 +1,19 @@
-
-import Mathlib
 import Mathlib.Data.Set.Defs
 import Mathlib.Data.Set.Basic
 import Mathlib.Topology.Defs.Filter
+import Mathlib.Topology.Order.Basic
 import Mathlib.Order.Filter.Basic
 
 open Topology
 open Filter
 
-lemma general_iic_not_open  {α : Type u} [TopologicalSpace α] [LinearOrder α] [OrderClosedTopology α] [PreconnectedSpace α]  (a: α) (ha: ¬ IsMax a) : ¬ IsOpen (Set.Iic a) := by
-  have is_closed: IsClosed (Set.Iic a) := isClosed_Iic
-  have nonempty: (Set.Iic a).Nonempty := by simp
+variable {α : Type u} {a: α} [LinearOrder α]
 
-  have ici_not_univ: Set.Iic a ≠ Set.univ := by
-    simp only [Set.ne_univ_iff_exists_not_mem, Set.mem_Iic, not_le]
-    apply not_isMax_iff.mp ha
+lemma Set.Iic.ne_univ_of_not_isMax (ha: ¬ IsMax a) : Set.Iic a ≠ Set.univ :=
+  (Set.ne_univ_iff_exists_not_mem _).mpr <| by simpa using not_isMax_iff.mp ha
 
-  have not_clopen: ¬IsClopen (Set.Iic a) := by
-    apply (not_imp_not.mpr isClopen_iff.mp)
-    rw [not_or]
-    exact ⟨Set.Nonempty.ne_empty nonempty, ici_not_univ⟩
+variable [TopologicalSpace α] [OrderClosedTopology α] [PreconnectedSpace α]
 
-  rw [IsClopen, not_and] at not_clopen
-  apply not_clopen is_closed
+lemma Iic_not_open (ha: ¬ IsMax a) : ¬ IsOpen (Set.Iic a) :=
+  (not_and.mp <| not_imp_not.mpr isClopen_iff.mp <| not_or.mpr <|
+    ⟨Set.Nonempty.ne_empty (by simp), Set.Iic.ne_univ_of_not_isMax ha⟩) isClosed_Iic
