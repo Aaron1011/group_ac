6e246eff08a0560bf96cb455cc850c1261909e3f
Aaron Hill <aa1ronham@gmail.com>
2024-10-14 15:06:52 -0400

    More work

--- GroupAc/NotOpen.lean
+++ GroupAc/NotOpen.lean
@@ -8,24 +8,26 @@ import Mathlib.Topology.Order.DenselyOrdered
 open Topology
 open Filter
 
-variable {α : Type u} {a b: α} [LinearOrder α] [TopologicalSpace α]
-
-lemma frontier_to_not_IsOpen [OrderTopology α] [DenselyOrdered α] [OrderClosedTopology α] [NoMaxOrder α] {s: Set α} (hb: b ∈ s) (hf: b ∈ frontier s): ¬ IsOpen s := by
+/-- A set with a point in both the frontier and the set is not open -/
+private lemma frontier_to_not_IsOpen {β: Type u} [TopologicalSpace β] {s: Set β} {a: β} (hb: a ∈ s) (hf: a ∈ frontier s): ¬ IsOpen s := by
   apply not_imp_not.mpr IsOpen.inter_frontier_eq
   simp_rw [Set.eq_empty_iff_forall_not_mem, Set.mem_inter_iff, not_forall, Classical.not_not]
-  refine ⟨b, ⟨hb, hf⟩⟩
+  refine ⟨a, ⟨hb, hf⟩⟩
 
-lemma Iic_not_open [OrderTopology α] [DenselyOrdered α] [NoMaxOrder α]: ¬ IsOpen (Set.Iic a) := by
+
+variable {α : Type u} {a b: α} [LinearOrder α] [TopologicalSpace α] [OrderTopology α]  [DenselyOrdered α]
+
+theorem not_IsOpen_Iic [NoMaxOrder α]: ¬ IsOpen (Set.Iic a) := by
   apply not_imp_not.mpr IsOpen.inter_frontier_eq
   rw [frontier_Iic, Set.inter_singleton_eq_empty, Set.mem_Iic, Classical.not_not]
 
-lemma Ici_not_open [OrderTopology α] [DenselyOrdered α] [NoMinOrder α] : ¬ IsOpen (Set.Ici a) := Iic_not_open (α := αᵒᵈ)
+theorem not_IsOpen_Ici [NoMinOrder α] : ¬ IsOpen (Set.Ici a) := not_IsOpen_Iic (α := αᵒᵈ)
 
-lemma Ioc_not_open [OrderTopology α] [DenselyOrdered α] [NoMaxOrder α] (hab: a < b): ¬ IsOpen (Set.Ioc a b) :=
-  frontier_to_not_IsOpen (Set.mem_Ioc.mpr ⟨hab, le_refl _⟩) (by simp [frontier_Ioc hab])
+theorem not_IsOpen_Ioc [NoMaxOrder α] (h: a < b): ¬ IsOpen (Set.Ioc a b) :=
+  frontier_to_not_IsOpen (Set.mem_Ioc.mpr ⟨h, le_refl _⟩) (by simp [frontier_Ioc h])
 
-lemma Ico_not_open [DenselyOrdered α] [NoMinOrder α] [OrderTopology α] (hab: a < b): ¬ IsOpen (Set.Ico a b) := by
-  simpa only [Set.Ioc, and_comm] using (Ioc_not_open (α := αᵒᵈ) (LT.lt.dual hab))
+theorem not_IsOpen_Ico [NoMinOrder α] (h: a < b): ¬ IsOpen (Set.Ico a b) := by
+  simpa only [Set.Ioc, and_comm] using (not_IsOpen_Ioc (α := αᵒᵈ) (LT.lt.dual h))
 
-lemma Icc_not_open [OrderTopology α] [DenselyOrdered α] [OrderClosedTopology α] [NoMinOrder α] [NoMaxOrder α] (hab: a ≤ b): ¬ IsOpen (Set.Icc a b) :=
-  frontier_to_not_IsOpen (Set.mem_Icc.mpr ⟨hab, le_refl _⟩) (by simp [frontier_Icc hab])
+theorem not_IsOpen_Icc [NoMinOrder α] [NoMaxOrder α] (h: a ≤ b): ¬ IsOpen (Set.Icc a b) :=
+  frontier_to_not_IsOpen (Set.mem_Icc.mpr ⟨h, le_refl _⟩) (by simp [frontier_Icc h])
