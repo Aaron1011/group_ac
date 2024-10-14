88fc55e4b64bb4d429dfc3ba9ce0adb20d83fb83
Aaron Hill <aa1ronham@gmail.com>
2024-10-13 21:32:44 -0400

    More golfing

--- GroupAc/NotOpen.lean
+++ GroupAc/NotOpen.lean
@@ -17,11 +17,9 @@ lemma Iic_not_open [OrderTopology α] [DenselyOrdered α] [NoMaxOrder α]: ¬ Is
 lemma Ici_not_open [OrderTopology α] [DenselyOrdered α] [NoMinOrder α] : ¬ IsOpen (Set.Ici a) := Iic_not_open (α := αᵒᵈ)
 
 lemma Ioc_not_open [OrderTopology α] [DenselyOrdered α] [NoMaxOrder α] (hab: a < b): ¬ IsOpen (Set.Ioc a b) := by
-  have hb: b ∈ (Set.Ioc a b) ∩ frontier (Set.Ioc a b) := by
-    rw [frontier_Ioc hab, Set.mem_inter_iff, Set.mem_insert_iff, Set.mem_singleton_iff, Set.mem_Ioc]
-    simpa only [le_refl, and_true, or_true]
-
-  exact not_imp_not.mpr IsOpen.inter_frontier_eq (Set.nonempty_iff_ne_empty.mp ⟨b, hb⟩)
+  apply not_imp_not.mpr IsOpen.inter_frontier_eq
+  simp_rw [frontier_Ioc hab, Set.eq_empty_iff_forall_not_mem, Set.mem_inter_iff, Set.mem_Ioc, not_forall, Classical.not_not]
+  refine ⟨b, ⟨hab, le_refl _⟩, by simp_rw [Set.mem_insert_iff, Set.mem_singleton_iff, or_true]⟩
 
 lemma Ico_not_open [DenselyOrdered α] [NoMinOrder α] [OrderTopology α] (hab: a < b): ¬ IsOpen (Set.Ico a b) := by
   simpa only [Set.Ioc, and_comm] using (Ioc_not_open (α := αᵒᵈ) (LT.lt.dual hab))
