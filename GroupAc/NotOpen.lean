66d30424bd53fbb41e8e369c385a9ffab15aaa3a
Aaron Hill <aa1ronham@gmail.com>
2024-10-13 21:01:20 -0400

    More golfing

--- GroupAc/NotOpen.lean
+++ GroupAc/NotOpen.lean
@@ -8,76 +8,23 @@ import Mathlib.Topology.Order.DenselyOrdered
 open Topology
 open Filter
 
-variable {α : Type u} {a: α} [LinearOrder α]
+variable {α : Type u} {a b: α} [LinearOrder α] [TopologicalSpace α]
 
-lemma Set.Iic.ne_univ_of_not_isMax (ha: ¬ IsMax a) : Set.Iic a ≠ Set.univ :=
-  (Set.ne_univ_iff_exists_not_mem _).mpr <| by simpa using not_isMax_iff.mp ha
-
-variable [TopologicalSpace α] [OrderTopology α]
-
-lemma Iic_not_open [DenselyOrdered α] [NoMaxOrder α]: ¬ IsOpen (Set.Iic a) := by
+lemma Iic_not_open [OrderTopology α] [DenselyOrdered α] [NoMaxOrder α]: ¬ IsOpen (Set.Iic a) := by
   simp only [Set.nonempty_Ioi, frontier_Iic', Set.inter_singleton_eq_empty, Set.mem_Iic, le_refl,
     not_true_eq_false, not_false_eq_true, not_imp_not.mpr IsOpen.inter_frontier_eq]
 
 
-   --simp [not_imp_not.mpr IsOpen.inter_frontier_eq, frontier_Iic]
+lemma Ici_not_open [OrderTopology α] [DenselyOrdered α] [NoMinOrder α] : ¬ IsOpen (Set.Ici a) := Iic_not_open (α := αᵒᵈ)
 
-
-instance instPreconnected (α : Type*) [TopologicalSpace α] [hq: PreconnectedSpace α] : PreconnectedSpace αᵒᵈ := ‹_›
-
-lemma Ici_not_open [DenselyOrdered α] [NoMinOrder α] : ¬ IsOpen (Set.Ici a) := Iic_not_open (α := αᵒᵈ)
-
--- Missing 'open Set' implies undefined univ, weird error here
-lemma not_preconnected {α: Type u} [TopologicalSpace α] (ha: ¬(PreconnectedSpace α)) : (¬ IsPreconnected (Set.univ : Set α)) := by
-  intro hz
-  have is_preconnected: PreconnectedSpace α := ⟨hz⟩
-  contradiction
-
-
--- (a, b] [a, b)
-
-lemma general_ioc_not_open {α: Type*} [TopologicalSpace α] [LinearOrder α] [DenselyOrdered α] [NoMaxOrder α] [OrderTopology α] (a b: α) (hab: a < b): ¬ IsOpen (Set.Ioc a b) := by
-  have frontier_eq: frontier (Set.Ioc a b) = {a, b} := by apply frontier_Ioc hab
-  have frontier_imp_not_open: ¬((Set.Ioc a b) ∩ frontier (Set.Ioc a b) = ∅) → ¬ IsOpen (Set.Ioc a b) := not_imp_not.mpr IsOpen.inter_frontier_eq
-  have b_in_ioc: b ∈ Set.Ioc a b := by
-    rw [Set.mem_Ioc]
-    refine ⟨hab, ?_⟩
-    simp
+lemma general_ioc_not_open [OrderTopology α] [DenselyOrdered α] [NoMaxOrder α] (hab: a < b): ¬ IsOpen (Set.Ioc a b) := by
   have intersect_frontier: b ∈ (Set.Ioc a b) ∩ frontier (Set.Ioc a b) := by
-    rw [frontier_eq]
-    simp [b_in_ioc]
+    rw [frontier_Ioc hab]
+    rw [Set.mem_inter_iff, Set.mem_insert_iff, Set.mem_singleton_iff, Set.mem_Ioc]
+    simp only [le_refl, and_true, or_true]
+    exact hab
 
-  have frontier_intersect_nonempty: Set.Nonempty ((Set.Ioc a b) ∩ frontier (Set.Ioc a b)):= by
-    refine ⟨b, intersect_frontier⟩
+  exact not_imp_not.mpr IsOpen.inter_frontier_eq (Set.nonempty_iff_ne_empty.mp ⟨b, intersect_frontier⟩)
 
-  have not_eq_empty: ¬((Set.Ioc a b) ∩ frontier (Set.Ioc a b) = ∅) := by
-    exact Set.nonempty_iff_ne_empty.mp frontier_intersect_nonempty
-
-  apply frontier_imp_not_open not_eq_empty
-
-lemma general_ico_not_open {α: Type*} [TopologicalSpace α] [LinearOrder α] [DenselyOrdered α] [NoMinOrder α] [OrderTopology α] (a b: α) (hab: a < b): ¬ IsOpen (Set.Ico a b) := by
-  have frontier_eq: frontier (Set.Ico a b) = {a, b} := by apply frontier_Ico hab
-  have frontier_imp_not_open: ¬((Set.Ico a b) ∩ frontier (Set.Ico a b) = ∅) → ¬ IsOpen (Set.Ico a b) := not_imp_not.mpr IsOpen.inter_frontier_eq
-  have a_in_ico: a ∈ Set.Ico a b := by
-    rw [Set.mem_Ico]
-    refine ⟨?_, hab⟩
-    simp
-  have intersect_frontier: a ∈ (Set.Ico a b) ∩ frontier (Set.Ico a b) := by
-    rw [frontier_eq]
-    simp [a_in_ico]
-
-  have frontier_intersect_nonempty: Set.Nonempty ((Set.Ico a b) ∩ frontier (Set.Ico a b)):= by
-    refine ⟨a, intersect_frontier⟩
-
-  have not_eq_empty: ¬((Set.Ico a b) ∩ frontier (Set.Ico a b) = ∅) := by
-    exact Set.nonempty_iff_ne_empty.mp frontier_intersect_nonempty
-
-  apply frontier_imp_not_open not_eq_empty
-
-lemma dual_general_ico_not_open {α: Type*}
-  [TopologicalSpace α] [LinearOrder α] [DenselyOrdered α] [NoMinOrder α] [OrderTopology α]
-  (a b: α) (hab: a < b): ¬ IsOpen (Set.Ico a b) := by
-    have something := general_ioc_not_open (α := αᵒᵈ) b a hab
-    simp only [Set.Ico, and_comm]
-    simp only [Set.Ioc, and_comm] at something
-    apply something
+lemma dual_general_ico_not_open [DenselyOrdered α] [NoMinOrder α] [OrderTopology α] (hab: a < b): ¬ IsOpen (Set.Ico a b) := by
+  simpa [Set.Ioc, and_comm] using (general_ioc_not_open (α := αᵒᵈ) (LT.lt.dual hab))
